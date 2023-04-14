eb8e8d26abc12b40cbb1374f4dce3d24b6ab4a5c
ivonamojsovska <ivonamojsovska15@gmail.com>
2023-04-14 15:55:33 -0700

    ctx added to tasklist

--- pages/tasksList/index.js
+++ pages/tasksList/index.js
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { AiOutlineCheckCircle } from 'react-icons/ai'
 import { BsTrash } from 'react-icons/bs'
 import { CiEdit } from 'react-icons/ci'
+import { getTodos } from "@/utils/actions";
 
 const TasksList = ({ todos }) => {
     const router = useRouter()
@@ -67,22 +68,21 @@ const TasksList = ({ todos }) => {
     );
 };
 
-// This page will eventually display the most up to date list of our dogs, so it should be server-side rendered. To designate that we will page is serversideprops!
-// export async function getServerSideProps(ctx) {
-//     const id = ctx.query.id
-//     console.log(id)
-//     const todo = JSON.parse(JSON.stringify(await getTodo(id)))
-//     console.log(todo)
 
-//     // This function should return an object with a props property with all the props we want for this page
-//     // keep in mind this function is run server-side
-//     return {
-//         props: {
-//             title: "Todo App - Main Page",
-//             description: "This page will show us all our Todo",
-//             todo
-//         },
-//     };
+export async function getServerSideProps(ctx) {
+
+    const todos = JSON.parse(JSON.stringify(await getTodos()))
+
+
+    // This function should return an object with a props property with all the props we want for this page
+    // keep in mind this function is run server-side
+    return {
+        props: {
+
+            todos
+        },
+    };
+}
 
 
 
